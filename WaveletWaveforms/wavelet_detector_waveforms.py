2363db48c99c5f34a9c276984bb213a679babf06
Matthew Digman <mcdigman@gmail.com>
2025-06-03 14:22:19 -0500

    created an abstract class for wavelet domain waveforms

--- WaveletWaveforms/wavelet_detector_waveforms.py
+++ WaveletWaveforms/wavelet_detector_waveforms.py
@@ -1,8 +1,11 @@
 """Store a sparse binary wavelet waveform."""
 
+from abc import ABC, abstractmethod
+from typing import override
+
 from LisaWaveformTools.linear_frequency_source import LinearFrequencyWaveformTime
 from LisaWaveformTools.lisa_config import LISAConstants
-from LisaWaveformTools.stationary_source_waveform import SourceParams
+from LisaWaveformTools.stationary_source_waveform import SourceParams, StationarySourceWaveform
 from WaveletWaveforms.sparse_waveform_functions import PixelTimeRange, SparseWaveletWaveform
 from WaveletWaveforms.taylor_time_coefficients import (
     WaveletTaylorTimeCoeffs,
@@ -13,7 +16,83 @@ from WaveletWaveforms.taylor_time_wavelet_funcs import wavemaket
 from WaveletWaveforms.wdm_config import WDMWaveletConstants
 
 
-class BinaryWaveletAmpFreqDT:
+class SparseWaveletSourceWaveform(ABC):
+    """Abstract base class for sparse wavelet waveforms."""
+    def __init__(self, params: SourceParams, wavelet_waveform: SparseWaveletWaveform, source_waveform: StationarySourceWaveform):
+        """Initialize the sparse wavelet waveform."""
+        self._consistent = False
+        self.params = params
+        self.wavelet_waveform = wavelet_waveform
+        self.source_waveform = source_waveform
+
+        # initialize to input parameters
+        self.update_params(params)
+
+    @property
+    def params(self) -> SourceParams:
+        """Get the current source parameters."""
+        return self._params
+
+    @params.setter
+    def params(self, params_in: SourceParams) -> None:
+        """Set the source parameters and update the waveform. If called directly without
+        calling `update_params`, the internal representation will not be consistent.
+        """
+        self.consistent = False
+        self._params = params_in
+
+    @abstractmethod
+    def _update_wavelet_waveform(self) -> None:
+        """Update the wavelet waveform to match the current parameters."""
+
+    def update_params(self, params_in: SourceParams) -> None:
+        """Update the internal wavelet representation to match the input parameters.
+        This will update both the source waveform and the wavelet waveform to match the new parameters.
+        """
+        # Store the new parameters
+        self.params = params_in
+
+        self.source_waveform.update_params(params_in)
+        self._update_wavelet_waveform()
+
+        # Set consistent to True after the update is complete
+        self.consistent = True
+
+    def get_unsorted_coeffs(self) -> SparseWaveletWaveform:
+        """Get wavelet coefficients in the order they are generated."""
+        if not self.consistent:
+            self.update_params(self.params)
+        return self.wavelet_waveform
+
+    @property
+    def consistent(self) -> bool:
+        """Check if the internal representation is consistent with the input parameters.
+
+        Returns
+        ----------
+        consistent : bool
+            Whether the internal representation is consistent with the input parameters.
+
+        """
+        return self._consistent
+
+    @consistent.setter
+    def consistent(self, consistent: bool) -> None:
+        """Set whether the internal representation is consistent with the input parameters.
+
+        Parameters
+        ----------
+        consistent : bool
+            Whether the internal representation is consistent with the input parameters.
+        """
+        self._consistent = consistent
+
+    def _update_source_waveform(self) -> None:
+        """Update the source waveform to match the current parameters."""
+        self.source_waveform.update_params(self.params)
+
+
+class BinaryWaveletAmpFreqDT(SparseWaveletSourceWaveform):
     """Store a sparse binary wavelet and update if for search."""
 
     def __init__(self, params: SourceParams, wc: WDMWaveletConstants, lc: LISAConstants, nt_lim_waveform: PixelTimeRange) -> None:
@@ -22,46 +101,30 @@ class BinaryWaveletAmpFreqDT:
         self.lc = lc
         self.nt_lim_waveform = nt_lim_waveform
 
-        self.params = params
+        # get the waveform
+        source_waveform = LinearFrequencyWaveformTime(
+            params, self.nt_lim_waveform, self.lc, self.wc,
+        )
+
+        # get a blank waveform in the sparse wavelet domain
+        # when consistent is set to True, it will be the correct waveform
+        wavelet_waveform_loc: SparseWaveletWaveform = get_empty_sparse_taylor_time_waveform(self.lc.nc_waveform, wc)
 
         # interpolation for wavelet taylor expansion
         self.taylor_time_table: WaveletTaylorTimeCoeffs = get_taylor_table_time(
             self.wc, cache_mode='skip', output_mode='skip',
         )
 
-        # get the waveform in frequency space
-        self.fwt = LinearFrequencyWaveformTime(
-            self.params, self.nt_lim_waveform, self.lc, self.wc,
-        )
+        super().__init__(params, wavelet_waveform_loc, source_waveform)
 
-        # get a blank waveform in the sparse wavelet domain
-        # when consistent is set it will be correct
-        self.wavelet_waveform: SparseWaveletWaveform = get_empty_sparse_taylor_time_waveform(self.lc.nc_waveform, wc)
-
-        self.consistent = False
-
-        # initialize to input parameters
-        self.update_params(params)
-
-    def update_params(self, params_in: SourceParams) -> None:
-        """Update the internal wavelet representation to match the input parameters"""
-        self.consistent = False
-        self.params = params_in
-
-        self.fwt.update_params(params_in)
-
+    @override
+    def _update_wavelet_waveform(self) -> None:
+        """Update the wavelet waveform to match the current parameters."""
         wavemaket(
             self.wavelet_waveform,
-            self.fwt.get_tdi_waveform(),
+            self.source_waveform.get_tdi_waveform(),
             self.nt_lim_waveform,
             self.wc,
             self.taylor_time_table,
             force_nulls=False,
         )
-        self.consistent = True
-
-    def get_unsorted_coeffs(self) -> SparseWaveletWaveform:
-        """Get coefficients in the order they are generated"""
-        if not self.consistent:
-            self.update_params(self.params)
-        return self.wavelet_waveform
