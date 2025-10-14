48138989b9e9265ab34c258064caa41da750a5d4
Matthew Digman <mcdigman@gmail.com>
2025-10-13 19:26:13 -0500

    Added minimum working ruff failure test case

---
+++ tests/ruff_test_failure.py
@@ -0,0 +1,13 @@
+"""Test that ruff fails"""
+
+
+import pytest
+
+# TODO whatever
+# TODO whatever 2
+
+# scaling on (Nf, Nt, dt, mult) in the second configuration
+@pytest.mark.skip
+def test_ruff_fails() -> None:
+    """A test case that causes ruff to have an infinite loop"""
+    return
